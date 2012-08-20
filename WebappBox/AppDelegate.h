1198cb47bb7faf812f1ef049d968f492b68604f4
Szu-Kai Hsu (brucehsu) <brucehsu@gmail.com>
2012-08-21 05:15:14 +0800

    [New] Quit on window close

--- WebappBox/AppDelegate.h
+++ WebappBox/AppDelegate.h
@@ -11,5 +11,5 @@
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
-
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication;
 @end
